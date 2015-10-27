ca1d0ee9933e7ce1ed28f21a619df78bf471b9b6
Katchan <no1fushi@live.jp>
2015-10-28 02:09:40 +0900

    fix プロビジョニング

--- sh/provision_mycent.sh
+++ sh/provision_mycent.sh
@@ -1,34 +1,22 @@
 #!/bin/sh
-
-#install
-	sudo yum update
-	#set
-	sudo yum install -y clamd
-	sudo yum -y install perl-Net-SSLeay
-	sudo yum -yinstall python-pip
-	#develop
-	sudo yum install -y ruby
-	sudo yum install -y nginx
-	sudo yum install -y php
-	sudo yum install -y php-fpm
-	sudo yum install -y php-mysql
-	sudo yum install -y git
-	sudo rpm -i http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
-	sudo rpm -i http://download.webmin.com/download/yum/webmin-1.770-1.noarch.rpm
-	sudo rpm -i http://downloads.mysql.com/archives/get/file/MySQL-client-5.6.20-1.linux_glibc2.5.x86_64.rpm
-	sudo rpm -i http://downloads.mysql.com/archives/get/file/MySQL-server-5.6.20-1.linux_glibc2.5.x86_64.rpm
-	sudo rpm -i http://downloads.mysql.com/archives/get/file/MySQL-shared-compat-5.6.20-1.linux_glibc2.5.x86_64.rpm
-	sudo rpm -i http://downloads.mysql.com/archives/get/file/MySQL-devel-5.6.20-1.linux_glibc2.5.x86_64.rpm
-	sudo yum install -y openssl shellinabox
-	sudo yum install -y vim
-	#fun
-	sudo yum install -y sl
-	sudo yum install -y cowsay
-	#tool
-	sudo yum install -y lynx
-	sudo pip install -y rainbowstream
-
-#setup
-	#clamd
-	sed -i 's/^Example/#Example/g' /etc/freshclam.conf
-	freshclam
+sudo yum update -y
+sudo wget http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+rpm -Uvh mysql-community-release-el7-5.noarch.rpm
+sudo yum -y install sl tree cowsay lynx vim ruby php php-fpm php-mysql perl perl-Net-SSLeay openssl perl-IO-Tty mysql-community-server nginx epel-release git clamd openssl shellinabox 
+sudo chkconfig clamd on
+sudo chkconfig --add nginx
+sudo chkconfig nginx on
+sudo chkconfig php-fpm on
+sudo chkconfig mysqld on
+sudo chkconfig shellinaboxd on
+sudo sed -i 's/^Example/#Example/g' /etc/freshclam.conf
+cd $HOME/
+wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/Vim/vimrc
+mv vimrc .vimrc
+sudo freshclam
+vim --version
+git --version
+ruby --version
+mysql --version
+nginx -v
+php -v
