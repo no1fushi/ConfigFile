32048a2ecc32698c7003a738684ed36227618947
Katchan <no1fushi@live.jp>
2015-12-12 02:08:46 +0900

    fix bat/testserver.bat runputty

--- bat/testserver.bat
+++ bat/testserver.bat
@@ -1,5 +1,5 @@
 @echo off
-cd C:\Users\%username%\centos
+cd C:\Users\%username%\testserver\
 
 :loop
 set /P Select="upかhaltを入力してください: "
@@ -7,12 +7,17 @@ set /P Select="upかhaltを入力してください: "
 if "%Select%" == "up" (
 @echo on
 vagrant up
+
+cd puttyのあるディレクトリ
+putty.exe
 ) else if "%Select%" == "halt" (
 @echo on
 vagrant halt
 ) else (
 call :loop
+
+endlocal
 exit \b
 )
 
-exit \b
+exit
