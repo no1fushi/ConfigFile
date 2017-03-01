27f4abb46f23107068a288c227d831605c144dbf
root <root@raspberrypi>
2017-03-02 01:15:30 +0900

    fix monit.sh

--- sh/monit.sh
+++ sh/monit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-count=`ps aux | grep ruby `
+count=`ps aux | grep ruby |wc -l`
 
 if [ $count = 0 ]; then
-	command
-	echo "body" | mail -s "sub" mail@mail.com
+	/home/pi/script/disbotstart
+	echo "disbot restart" | mail -s "disbot downt" no1fushi@gmail.com
 	else
-		echo "ok,green"
+		:
 fi
