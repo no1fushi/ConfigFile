bbe8219e7d2df484a0d0e4e6dddb4beefdb4a9d1
root <root@raspberrypi>
2017-03-02 00:17:09 +0900

    hidden mail

--- sh/monit.sh
+++ sh/monit.sh
@@ -3,8 +3,8 @@
 count=`ps aux | grep ruby `
 
 if [ $count = 0 ]; then
-	/home/pi/script/disbotstart
-	echo "disbot restart" | mail -s "disbot downt" no1fushi@gmail.com
+	command
+	echo "body" | mail -s "sub" mail@mail.com
 	else
-		echo "ok"
+		echo "ok,green"
 fi
