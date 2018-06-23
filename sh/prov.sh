bfcc47246399ed540f92719d21a7fd9fb521c3d7
Katou Fuushi <no1fushi@live.jp>
2018-06-24 05:54:10 +0900

    fix prov.sh

--- sh/prov.sh
+++ sh/prov.sh
@@ -1,124 +1,142 @@
 #!/bin/bash
 
+#Questions
+echo "Will ask you a couple of questions first"
+echo "Please answer yes or no"
+echo "These questions are set to no by default"
+
+echo "Will modify the files that think are necessary and install the software"
+read -p "Do you allow it? [yes/no]: " do
+if [ "$do" == "yes" ] || [ "$do" == "y" ] || [ "$do" == "YES" ] || [ "$do" == "Y" ] || [ "$do" == "Yes" ]; then
+
+	read -p "Do you want ssh-keygen? [yes/no]: " ssh
+	read -p "Do you want to install Libsodium? [yes/no]: " libsodium
+	read -p "Do you want to install joke softwares? [yes/no]: " joke
+	read -p "Do you need a script created by no1fushi? [yes/no]: " script
+
+	echo "The question is finished"
+	echo "So run the provisioning script"
+
 #Apt
-yes | sudo apt-get update
-yes | sudo apt-get upgrade
-yes | sudo apt-get dist-upgrade
+	yes | sudo apt-get update
+	yes | sudo apt-get upgrade
+	yes | sudo apt-get dist-upgrade
+	yes | sudo apt-get update
 
-#ssh
-ssh-keygen -t rsa
-mv ~/id_rsa.pub ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-
+	#ssh
+	if [ "$ssh" == "yes" ] || [ "$ssh" == "y" ] || [ "$ssh" == "YES" ] || [ "$ssh" == "Y" ] || [ "$ssh" == "Yes" ]; then
+		ssh-keygen -t rsa
+		mv ~/id_rsa.pub ~/.ssh/authorized_keys
+		chmod 600 ~/.ssh/authorized_keys
+	fi
 #Lang
-yes | sudo apt-get install gcc gauche
-yes | sudo apt-get install node npm
+	yes | sudo apt-get install gcc gauche
+	yes | sudo apt-get install node npm
 
 #Editor
-yes | sudo apt-get install vim emacs24
+	yes | sudo apt-get install vim emacs24
 
 #Tool
-yes | sudo apt-get install tree curl
-yes | sudo apt-get install git
-yes | sudo apt-get install fortune
+	yes | sudo apt-get install tree curl
+	yes | sudo apt-get install git
+	yes | sudo apt-get install fortune
 
 #Ruby
-apt-get install gcc-6 autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-exec $SHELL -l
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-rbenv install 2.4.1
-rbenv rehash
-rbenv global 2.4.1
+	yes | sudo apt-get install gcc-6 autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+	sudo git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+	sudo chmod a=rwx .rbenv
+	echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+	echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+	source .bash_profile
+	sudo git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+	rbenv install 2.4.1
+	rbenv rehash
+	rbenv global 2.4.1
 
 #RootMail
-yes | sudo apt-get install ssmtp sendmail-base
+	yes | sudo apt-get install ssmtp sendmail-base
 
 #Logwatch
-yes | sudo apt-get install logwatch
+	yes | sudo apt-get install logwatch
 
-#Meteor
-yes | sudo apt-get install g++ build-essential debian-keyring autoconf automake libtool flex bison scons git mongodb
-$HOME/meteor/meteor --version
+#Mongodb
+	yes | sudo apt-get install g++ build-essential debian-keyring autoconf automake libtool flex bison scons git mongodb
 
 #GemPackages
-yes | gem install discordrb gmail net-ping bigdecimal
+	yes | gem install discordrb gmail net-ping bigdecimal
 
 #Libsodium
-wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.15.tar.gz
-tar -zxvf libsodium-1.0.15.tar.gz
-rm -rf libsodium-1.0.15
-cd libsodium-1.0.15
-./configure
-make && make check
-sudo make install
-cd
-rm -rf libsodium-1.0.15/
- 
+	if [ "$libsodium" == "yes" ] || [ "$libsodium" == "y" ] || [ "$libsodium" == "YES" ] || [ "$libsodium" == "Y" ] || [ "$libsodium" == "Yes" ]; then
+		wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.15.tar.gz	tar -zxvf libsodium-1.0.15.tar.gz
+		rm -rf libsodium-1.0.15.tar.gz
+		cd libsodium-1.0.15
+		./configure
+		make && make check
+		sudo make install
+		cd
+		rm -rf libsodium-1.0.15/
+	fi
 #Joke
-yes | sudo apt-get install sl cowsay
-yes | gem install cureutils matsuya
-yes | gem install rubipara takarabako
-yes | sudo npm install -g faker-cli emspect
-yes | sudo apt-get install screenfetch
+if [ "$joke" == "yes" ] || [ "$joke" == "y" ] || [ "$joke" == "YES" ] || [ "$joke" == "Y" ] || [ "$joke" == "Yes" ]; then
+		yes | sudo apt-get install sl cowsay
+		yes | gem install cureutils matsuya
+		yes | gem install rubipara takarabako
+		yes | sudo npm install -g faker-cli emspect
+		yes | sudo apt-get install screenfetch
+fi
 
 #SetupEditor
-wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/Vim/.vimrc
-cd ~/.emacs.d
-wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/emacs/init.el
-sudo su
-cd
-wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/Vim/.vimrc
-mkdir .emacs.d
-cd ~/.emacs.d
-wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/emacs/init.el
-exit
-cd
+	wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/Vim/.vimrc
+	wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/emacs/init.el
+	mkdir .emacs.d
+	mv init.el ~/.emacs.d/
+	cd
 
 #Setup .bash_profile
-echo 'source ~/.bashrc' >> ~/.bash_profile
-echo 'echo -e "\n Disk capacity"' >> ~/.bash_profile
-echo 'df -h' >> ~/.bash_profile
-echo 'echo -e "\n"' >> ~/.bash_profile
-echo 'screenfetch' >> ~/.bash_profile
-echo 'echo -e "\n Users logged in at the same time"' >> ~/.bash_profile
-echo 'w' >> ~/.bash_profile
-echo 'echo -e "\n Confirming the Discord Bot"' >> ~/.bash_profile
-echo 'ps ax | grep ruby' >> ~/.bash_profile
+	echo 'source ~/.bashrc' >> ~/.bash_profile
+	echo 'echo -e "\n Disk capacity"' >> ~/.bash_profile
+	echo 'df -h' >> ~/.bash_profile
+	echo 'echo -e "\n"' >> ~/.bash_profile
+	echo 'screenfetch' >> ~/.bash_profile
+	echo 'echo -e "\n Users logged in at the same time"' >> ~/.bash_profile
+	echo 'w' >> ~/.bash_profile
 
 #Setup .bashrc
-rm .bashrc
-wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/bash/.bashrc
-source ~/.bashrc
-sudo su
-cd
-rm .bashrc
-wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/bash/.bashrc
-source ~/.bashrc
-exit
-cd
+	rm .bashrc
+	wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/bash/.bashrc
+	source ~/.bashrc
 
 #SetupFile
-mkdir script
-echo 'export PATH="$HOME/script:$PATH"' >> ~/.bash_profile
-cd script/
-mkdir tmp
-touch tmp/old_ip.txt
-echo 'a' >> tmp/old_ip.txt
-yes | sudo apt-get install mutt
-wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/sh/mc
-wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/sh/monit.sh
-wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/sh/sendip
-chmod +x monit.sh
-chmod +x sendip
-touch chruby
-echo 'ps ax | grep ruby' >> chruby
-chmod +x chmod
-cd
+	if [ "$script" == "yes" ] || [ "$script" == "y" ] || [ "$script" == "YES" ] || [ "$script" == "Y" ] || [ "$script" == "Yes" ]; then
+		mkdir script
+		echo 'export PATH="$HOME/script:$PATH"' >> ~/.bash_profile
+		mkdir script/tmp/
+		touch script/tmp/old_ip.txt
+		echo 'a' >> script/tmp/old_ip.txt
+		yes | sudo apt-get install mutt
+		wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/sh/mc
+		wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/sh/monit.sh
+		wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/sh/sendip
+		chmod +x monit.sh
+		chmod +x sendip
+		chmod +x mc
+		mv monit.sh script/
+		mv sendip script/
+		mv mc script/
+		touch script/chruby
+		echo 'ps ax | grep ruby' >> script/chruby
+		chmod +x script/chruby
+	fi
+
+	sudo cp .bash_profile /root/
+	sudo cp .bashrc /root/
+	sudo cp -r .emacs.d/ /root/
+	sudo cp .vimrc /root/
 
 #Apt-clean
-sudo apt-get autoremove --purge
-sudo apt-get clean
+	yes | sudo apt-get autoremove --purge
+	yes | sudo apt-get clean
 
+else
+	exit
+fi
