d3b5ba56d3275b65bf930f5653732804829a77fa
no1fushi <no1fushi@live.jp>
2019-11-08 15:36:55 +0000

    update prov_ubuntu.sh

--- sh/prov_ubuntu.sh
+++ sh/prov_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Questions
+# Questions
 echo "Will ask you a couple of questions first"
 echo "Please answer yes or no"
 echo "These questions are set to no by default"
@@ -11,46 +11,47 @@ if [ $do = "yes" ] || [ $do = "y" ] || [ $do = "YES" ] || [ $do = "Y" ] || [ $do
 
 	read -p "Do you want ssh-keygen? [yes/no]: " ssh
 	read -p "Do you want to install joke softwares? [yes/no]: " joke
-	read -p "Do you need a script created by no1fushi? [yes/no]: " script
 
 	echo "The question is finished"
 	echo "So run the provisioning script"
 
-#Apt
-	yes | sudo apt-get update
-	yes | sudo apt-get upgrade
-	yes | sudo apt-get dist-upgrade
-	yes | sudo apt-get update
+# Apt
+	yes | sudo apt update
+	yes | sudo apt upgrade
+	yes | sudo apt dist-upgrade
+	yes | sudo apt update
 
-	#ssh
+# ssh
 	if [ $ssh = "yes" ] || [ $ssh = "y" ] || [ $ssh = "YES" ] || [ $ssh = "Y" ] || [ $ssh = "Yes" ]; then
 		ssh-keygen -t rsa
 		mv ~/id_rsa.pub ~/.ssh/authorized_keys
 		chmod 600 ~/.ssh/authorized_keys
 	fi
 
-#Lang
-	yes | sudo apt-get install gcc gauche
-	yes | sudo apt-get install nodejs npm
-	yes | sudo apt-get install default-jre default-jdkc
+# Lang
+	yes | sudo apt install gcc gauche
+	yes | sudo apt install nodejs npm
+	yes | sudo apt install default-jre default-jdkc
 
-#Editor
-	yes | sudo apt-get install vim emacs24
+# Editor
+	yes | sudo apt install vim emacs24
 
-#SetupEditor
+# Tool
+	yes | sudo apt install tree traceroute unzip fortune tmux
+	yes | sudo apt install git wget curl
+
+# SetupEditor
+	rm .bashrc
+	rm .bash_profile
 	wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/Vim/.vimrc
 	wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/emacs/init.el
+	wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/bash/.bash_profile
+	wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/bash/.bashrc
 	mkdir .emacs.d
 	mv init.el ~/.emacs.d/
 
-#Tool
-	yes | sudo apt-get install tree curl traceroute
-	yes | sudo apt-get install git
-	yes | sudo apt-get install fortune
-	yes | sudo apt-get install tmux
-
-#Ruby
-	yes | sudo apt-get install gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+# Ruby
+	yes | sudo apt install gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 	sudo git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 	sudo chmod a=rwx .rbenv
 	echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
@@ -61,71 +62,56 @@ if [ $do = "yes" ] || [ $do = "y" ] || [ $do = "YES" ] || [ $do = "Y" ] || [ $do
 	rbenv rehash
 	rbenv global 2.4.1
 
-#RootMail
-	yes | sudo apt-get install ssmtp sendmail-base
+# Python
+	yes | sudo apt install -y build-essential libffi-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev
+	sudo git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+	echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+	source ~/.bash_profile
+	pyenv install 3.8.0
+	pyenv global 3.8.0
 
-#Logwatch
-	yes | sudo apt-get install logwatch
+# Python packages
+	pip install --upgrade pip
+	pip install thefuck
+	echo eval $(thefuck --alias) >> ~/.bash_profile
+	echo eval $(thefuck --alias) >> ~/.bash_rc
+	source ~/.bash_profile
+	source ~/.bashrc
+	pip install numpy pandas matplotlib
 
-#Mongodb
-	yes | sudo apt-get install g++ build-essential debian-keyring autoconf automake libtool flex bison scons git mongodb
+# RootMail
+	yes | sudo apt install ssmtp sendmail-base
 
-#GemPackages
-	yes | gem install discordrb gmail net-ping bigdecimal flumtter
+# Logwatch
+	yes | sudo apt install logwatch
 
-#Joke
+# Mongodb
+	yes | sudo apt install g++ build-essential debian-keyring autoconf automake libtool flex bison scons git mongodb
+
+# Joke
 if [ $joke = "yes" ] || [ $joke = "y" ] || [ $joke = "YES" ] || [ $joke = "Y" ] || [ $joke = "Yes" ]; then
-		yes | sudo apt-get install sl cowsay
+		yes | sudo apt install sl cowsay
 		yes | gem install cureutils matsuya
 		yes | gem install rubipara takarabako
 		yes | sudo npm install -g faker-cli emspect
-		yes | sudo apt-get install screenfetch
-		echo 'screenfetch' >> ~/.bash_profile
+		yes | sudo apt install screenfetch
 fi
 
-#Setup .bash_profile
-	echo 'source ~/.bashrc' >> ~/.bash_profile
-	echo 'echo -e "\n Disk capacity"' >> ~/.bash_profile
-	echo 'df -h' >> ~/.bash_profile
-	echo 'echo -e "\n"' >> ~/.bash_profile
-	echo 'echo -e "\n Users logged in at the same time"' >> ~/.bash_profile
-	echo 'w' >> ~/.bash_profile
 #Setup .bashrc
-	rm .bashrc
-	wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/soft/bash/.bashrc
 	source ~/.bashrc
-
-#SetupFile
-	if [ $script = "yes" ] || [ $script = "y" ] || [ $script = "YES" ] || [ $script = "Y" ] || [ $script = "Yes" ]; then
-		mkdir script
-		echo 'export PATH="$HOME/script:$PATH"' >> ~/.bash_profile
-		mkdir script/tmp/
-		touch script/tmp/old_ip.txt
-		echo 'a' >> script/tmp/old_ip.txt
-		yes | sudo apt-get install mutt
-		wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/sh/mc
-		wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/sh/monit.sh
-		wget https://raw.githubusercontent.com/no1fushi/ConfigFile/master/sh/sendip
-		chmod +x monit.sh
-		chmod +x sendip
-		chmod +x mc
-		mv monit.sh script/
-		mv sendip script/
-		mv mc script/
-		touch script/chruby
-		echo 'ps ax | grep ruby' >> script/chruby
-		chmod +x script/chruby
-	fi
-
+	source ~/.bash_profile
 	sudo cp .bash_profile /root/
 	sudo cp .bashrc /root/
 	sudo cp -r .emacs.d/ /root/
 	sudo cp .vimrc /root/
 
 #Apt-clean
-	yes | sudo apt-get autoremove --purge
-	yes | sudo apt-get clean
+	yes | sudo apt autoremove --purge
+	yes | sudo apt clean
 
 else
 	exit
 fi
+
